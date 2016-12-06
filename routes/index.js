0d881fe388cc336c95543a85ff2b24ba87bccd15
Ari <ari@Aris-MBP-2.fios-router.home>
2016-12-06 15:39:17 -0500

    working on api

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,14 @@ router.get('/', function(req, res) {
     res.sendFile('../public/index.html')
 });
 
+//get crimes in range
+//sends city-wide summary aswell
 router.get('/api/crimes', repository.getCrimesInRange);
 
+//on click sends tract summary
 router.get('/api/crimes/:tractId', repository.getTractSummary);
+
+//a new filter has been chosen, sends GIDs that dont match up
 router.get('/api/filter', repository.getFilterGIDs)
 
 module.exports = router;
