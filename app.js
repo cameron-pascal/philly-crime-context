73f0111897d513dfde73e8c834af5da6923dac4b
camtotheron <cpascal22@gmail.com>
2016-12-07 08:02:19 -0500

    added favicon

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-//var favicon = require('serve-favicon');
+const favicon = require('serve-favicon');
 const logger = require('morgan');
 const compression = require('compression');
 
@@ -8,8 +8,7 @@ const routes = require('./routes/index');
 
 const app = express();
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(compression());
